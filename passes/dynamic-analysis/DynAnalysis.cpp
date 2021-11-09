f8c62fc55ed54b49fbd06d41514efd9e303402e4
Souradip Ghosh <souradip@cmu.edu>
2021-11-09 14:49:33 +0000

    [pass][dynamic-analysis]: add FunctionPass setup

---
+++ passes/dynamic-analysis/DynAnalysis.cpp
@@ -0,0 +1,57 @@
+#include "include/Utils.hpp"
+
+namespace
+{
+struct CAT : public FunctionPass
+{
+    static char ID;
+
+    CAT() : FunctionPass(ID) {}
+
+    bool doInitialization(Module &M) override 
+    {   
+        /*
+         * Debugging setting
+         */ 
+        Utils::ExitOnInit();
+
+
+        return false;
+    }
+
+
+    bool runOnFunction(Function &F) override 
+    {
+        /*
+         * Check if the function should be handled or not
+         */
+        if (!Utils::IsViableFunction(F)) return false; 
+
+        
+        /*
+         * Status
+         */
+        bool Modified = false;
+        return Modified;
+    }
+
+
+    void getAnalysisUsage(AnalysisUsage &AU) const override 
+    {
+        return;
+    }
+
+};
+}
+
+
+char CAT::ID = 0;
+static RegisterPass<CAT> X("dynamic-analysis", "849 -- Dynamic analysis");
+
+static CAT* _PassMaker = NULL;
+static RegisterStandardPasses _RegPass1(PassManagerBuilder::EP_OptimizerLast,
+    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
+        if (!_PassMaker) { PM.add(_PassMaker = new CAT()); }}); 
+static RegisterStandardPasses _RegPass2(PassManagerBuilder::EP_EnabledOnOptLevel0,
+    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
+        if (!_PassMaker) { PM.add(_PassMaker = new CAT()); }});
